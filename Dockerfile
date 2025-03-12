1b1ff54ad51e3a175e5f0de66e3f6bd9e70c4569
Shubham <shubham_991@icloud.com>
2025-03-12 16:22:19 +0530

    re-commit

--- Dockerfile
+++ Dockerfile
@@ -42,7 +42,6 @@ COPY ./src /code
 
 # Install the Python project requirements
 RUN pip install -r /tmp/requirements.txt
-RUN pip install gunicorn
 
 # database isn't available during build
 # run any other commands that do not need the database
@@ -58,7 +57,7 @@ ARG PROJ_NAME="cfehome"
 RUN printf "#!/bin/bash\n" > ./paracord_runner.sh && \
     printf "RUN_PORT=\"\${PORT:-8000}\"\n\n" >> ./paracord_runner.sh && \
     printf "python manage.py migrate --no-input\n" >> ./paracord_runner.sh && \
-    printf "gunicorn ${PROJ_NAME}.wsgi:application --bind \"[::]:\$RUN_PORT\"\n" >> ./paracord_runner.sh
+    printf "gunicorn ${PROJ_NAME}.wsgi:application --bind \"0.0.0.0:\$RUN_PORT\"\n" >> ./paracord_runner.sh
 
 # make the bash script executable
 RUN chmod +x paracord_runner.sh
--- src/cfehome/urls.py
+++ src/cfehome/urls.py
@@ -1,8 +1,8 @@
 """
-URL configuration for vix project.
+URL configuration for cfehome project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -16,11 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+
 from .views import home_view, about_view
 
 urlpatterns = [
-    path("", home_view),
+    path("", home_view), #index page -> root page
     path("about/", about_view),
     path("hello-world/", home_view),
-    path('admin/', admin.site.urls),
+    path("hello-world.html", home_view),
+    path("admin/", admin.site.urls),
 ]
